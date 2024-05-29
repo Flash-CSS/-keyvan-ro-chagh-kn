496f89eccc7d669660d2824999e9eb6f82072ff5
Amir <Amir@AMIR-LAPTOP>
2024-05-29 11:21:52 +0330

    Add project files.

---
+++ k1ro chagh kn/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿using System.Windows.Forms;
+
+namespace PictureBoxSizeChanger
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private PictureBox pictureBox1;
+        private Button buttonIncreaseSize;
+        private Button buttonDecreaseSize;
+        private Button buttonSelectImage;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonIncreaseSize = new System.Windows.Forms.Button();
+            this.buttonDecreaseSize = new System.Windows.Forms.Button();
+            this.buttonSelectImage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(305, 220);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 240);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonIncreaseSize
+            // 
+            this.buttonIncreaseSize.FlatAppearance.BorderSize = 0;
+            this.buttonIncreaseSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonIncreaseSize.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIncreaseSize.Location = new System.Drawing.Point(13, 697);
+            this.buttonIncreaseSize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonIncreaseSize.Name = "buttonIncreaseSize";
+            this.buttonIncreaseSize.Size = new System.Drawing.Size(218, 51);
+            this.buttonIncreaseSize.TabIndex = 1;
+            this.buttonIncreaseSize.Text = "کیوان رو چاق کن";
+            this.buttonIncreaseSize.UseVisualStyleBackColor = true;
+            this.buttonIncreaseSize.Click += new System.EventHandler(this.buttonIncreaseSize_Click);
+            // 
+            // buttonDecreaseSize
+            // 
+            this.buttonDecreaseSize.FlatAppearance.BorderSize = 0;
+            this.buttonDecreaseSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDecreaseSize.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecreaseSize.Location = new System.Drawing.Point(618, 694);
+            this.buttonDecreaseSize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonDecreaseSize.Name = "buttonDecreaseSize";
+            this.buttonDecreaseSize.Size = new System.Drawing.Size(213, 51);
+            this.buttonDecreaseSize.TabIndex = 2;
+            this.buttonDecreaseSize.Text = "کیوان رو لاغر کن";
+            this.buttonDecreaseSize.UseVisualStyleBackColor = true;
+            this.buttonDecreaseSize.Click += new System.EventHandler(this.buttonDecreaseSize_Click);
+            // 
+            // buttonSelectImage
+            // 
+            this.buttonSelectImage.FlatAppearance.BorderSize = 0;
+            this.buttonSelectImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelectImage.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelectImage.Location = new System.Drawing.Point(305, 694);
+            this.buttonSelectImage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSelectImage.Name = "buttonSelectImage";
+            this.buttonSelectImage.Size = new System.Drawing.Size(260, 51);
+            this.buttonSelectImage.TabIndex = 3;
+            this.buttonSelectImage.Text = "کیوان رو انتخواب کن";
+            this.buttonSelectImage.UseVisualStyleBackColor = true;
+            this.buttonSelectImage.Click += new System.EventHandler(this.buttonSelectImage_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(819, 93);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "کیوان رو چاق کن";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 758);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSelectImage);
+            this.Controls.Add(this.buttonDecreaseSize);
+            this.Controls.Add(this.buttonIncreaseSize);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "کیوان رو چاق کن";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private Label label1;
+    }
+}
---
+++ k1ro chagh kn/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PictureBoxSizeChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            label1.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private void buttonIncreaseSize_Click(object sender, EventArgs e)
+        {
+            // Increase the size of the PictureBox from the center
+            int delta = 20;
+            pictureBox1.Size = new Size(pictureBox1.Width + delta, pictureBox1.Height + delta);
+            pictureBox1.Location = new Point(pictureBox1.Location.X - delta / 2, pictureBox1.Location.Y - delta / 2);
+
+            // Check if the PictureBox size exceeds the form size
+            if (pictureBox1.Right > this.ClientSize.Width || pictureBox1.Bottom > this.ClientSize.Height)
+            {
+                MessageBox.Show("😨کیوان دیگه چاق تر نیمشه😨", "👌تو برنده شدی👌", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonDecreaseSize_Click(object sender, EventArgs e)
+        {
+            // Decrease the size of the PictureBox from the center
+            MessageBox.Show("🥵کیوان که نمیتونه لاغر شه🥵", "!!!!ریدی");
+        }
+
+        private void buttonSelectImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                pictureBox1.Size = new Size(100, 100); // Set to a desired small size
+                pictureBox1.Location = new Point((this.ClientSize.Width - pictureBox1.Width) / 2, (this.ClientSize.Height - pictureBox1.Height) / 2); // Center the PictureBox
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
